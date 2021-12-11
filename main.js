23f01c9bd2751d99b86e1d6709d1a26d7341d1c9
DizzlyD <75099025+DizzlyD@users.noreply.github.com>
2021-12-11 16:10:54 +0000

    Update main.js

--- main.js
+++ main.js
@@ -291,31 +291,4 @@ function checkAll(myCheckBox) {
 //   }, delayAfter);
 // })
 /////////////
-//LINE GLITCH
-drawLines();
-function getHeight() {
-  return (
-    //
-    document.documentElement.clientHeight || document.body.clientHeight
-  );
-}
-function drawLines() {
-  const lines = document.getElementsByClassName("line");
-  if (lines.length) {
-    for (let i = 0; i < lines.length; i++) {
-      document.querySelector(".drop").removeChild(lines[i]);
-    }
-  }
-  for (let i = 0; i < getHeight() / 10; i++) {
-    const line = document.createElement("div");
-    line.className = `line line-${i}`;
-    line.style.top = `${i * 10}px`;
-    const time = Math.random() * 5;
-    line.style.animation = `lines ${time}s infinite`;
-    document.querySelector(".drop").appendChild(line);
-  }
-}
-window.onresize = function (event) {
-  drawLines();
-};
 /////////////////////////////////////////////
